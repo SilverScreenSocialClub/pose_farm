183b7301fb32d311a1b9d08ba10bc4902a7b6821
tale-fau <tale-fau@24eme.fr>
2024-11-03 11:24:04 +0100

    Commit project

---
+++ commands/utility/drop.js
@@ -0,0 +1,280 @@
+const zones =
+{
+  "plaines herbeuses": [
+      {
+        "name": "Zone : Plaines herbeuses",
+        "coord": "Coordonnées : [-50,13]",
+        "infos": "Ressource mob",
+        "map": "assets/plaines_herbeuse.png"
+      }
+  ],
+  "jungle obscure": [
+    {
+      "name": "Zone : Jungle obscure",
+      "coord": "Coordonnées : [-52,10]",
+      "infos": "Ressource mob",
+      "map": "assets/jungle_obscure.png"
+    }
+  ],
+  "péninsule des gelées": [
+    {
+      "name": "Zone : Péninsule des gelées",
+      "coord": "Coordonnées : [10,28]",
+      "infos": "Kamas brut, item gelée, maitrise pelle, ressource craft gelebottes",
+      "map": "assets/peninsule_des_gelees.png"
+    }
+  ],
+  "l'île de grobe": [
+    {
+      "name": "Zone : L'île de Grobe",
+      "coord": "Coordonnées : [33,-53]",
+      "infos": "Ressource mob",
+      "map": "assets/l_ile_de_grobe.png"
+    }
+  ],
+  "village des zoths": [
+    {
+      "name": "Zone : Village des Zoths",
+      "coord": "Coordonnées : [-54,16]",
+      "infos": "Ressource mob",
+      "map": "assets/village_des_zoths.png"
+    }
+  ],
+  "feuillage de l'arbre hakam": [
+    {
+      "name": "Zone : Feuillage de L'arbre Hakam",
+      "coord": "Coordonnées : [-54,15]",
+      "infos": "Ressource mob",
+      "map": "assets/feuillage_de_l_arbre_hakam.png"
+    }
+  ],
+  "bordure d'aerdala": [
+    {
+      "name": "Zone : Bordure d'Aerdala",
+      "coord": "Coordonnées : [17,-37]",
+      "infos": "GaPA, GaPM, rune en tout genre ( pandit et pandikaze)",
+      "map": "assets/bordure_d_aerdala.png"
+    }
+  ],
+  "la plage de moon": [
+    {
+      "name": "Zone : La plage de Moon",
+      "coord": "Coordonnées : [33,10]",
+      "infos": "Noix de koko, carapace tortue, ressource dora bora ",
+      "map": "assets/la_plage_de_moon.png"
+    }
+  ],
+  "caverne des fungus": [
+    {
+      "name": "Zone : Caverne des Fungus",
+      "coord": "Coordonnées : [-18,33]",
+      "infos": "Ressource mob",
+      "map": "assets/caverne_des_fungus.png"
+    }
+  ],
+  "vallée de la morh'kitu": [
+    {
+      "name": "Zone : Vallée de la Morh'Kitu",
+      "coord": "Coordonnées : [-16,13]",
+      "infos": "Boomerang koalak sanguin",
+      "map": "assets/vallee_de_la_morh_kitu.png"
+    }
+  ],
+  "territoire des bandits": [
+    {
+      "name": "Zone : Territoire des Bandits",
+      "coord": "Coordonnées : [12,18]",
+      "infos": "Parcho métiers, pierre brute, kamas brut, bougie ( 1000 kamas /u )",
+      "map": "assets/territoire_des_bandits.png"
+    }
+  ],
+  "la foret d'amakna": [
+    {
+      "name": "Zone : La foret d'amakna",
+      "coord": "Coordonnées : [8,14]",
+      "infos": "GaPA (drop abracaska , craft arc en racine abra et abracaska), kamas brut et items a vendre au pnj",
+      "map": "assets/la_foret_d_amakna.png"
+    }
+  ],
+  "foret des abraknydes": [
+    {
+      "name": "Zone : Foret des Abraknydes",
+      "coord": "Coordonnées : [-11,-16]",
+      "infos": "GaPA (drop abracaska , craft arc en racine abra et abracaska), kamas brut et items a vendre au pnj",
+      "map": "assets/foret_des_abraknydes.png"
+    }
+  ],
+  "l'île du minotoror": [
+    {
+      "name": "Zone : L'île du Minotoror",
+      "coord": "Coordonnées : [-43,-19]",
+      "infos": "Ressource clefs toror/tot",
+      "map": "assets/l_ile_du_minotoror.png"
+    }
+  ],
+  "plaine des porkass": [
+    {
+      "name": "Zone : Plaine des Porkass",
+      "coord": "Coordonnées : [-3,-29]",
+      "infos": "Ressource porkass, clef DC, clef ilyzaelle",
+      "map": "assets/plaine_des_porkass.png"
+    }
+  ],
+  "la montagne des craqueleurs": [
+    {
+      "name": "Zone : La montagne des Craqueleurs",
+      "coord": "Coordonnées : [-4,-6]",
+      "infos": "Pierre diamant, item bwak(500k/u), item craqueleur,kamas d'item et kamas brut",
+      "map": "assets/la_montagne_des_craqueleurs.png"
+    }
+  ],
+  "campement des bworks": [
+    {
+      "name": "Zone : Campement des bworks",
+      "coord": "Coordonnées : [-1,8]",
+      "infos": "Ficelle en lin ( craft arc en racine abra) , parcho métiers",
+      "map": "assets/campement_des_bworks.png"
+    }
+  ],
+  "sous terrain des dragoeufs": [
+    {
+      "name": "Zone : Sous terrain des Dragoeufs",
+      "coord": "Coordonnées : [-3,25]",
+      "infos": "Drop clefs et craft clefs Sanctuaire des dragoeufs",
+      "map": "assets/sous_terrain_des_dragoeufs.png"
+    }
+  ],
+  "sanctuaire des dragoeufs": [
+    {
+      "name": "Zone : Sanctuaire des Dragoeufs",
+      "coord": "Coordonnées : [-6,27]",
+      "infos": "Drop clefs et craft clefs Donjon des dragoeufs",
+      "map": "assets/sanctuaire_des_dragoeufs.png"
+    }
+  ],
+  "plaine des scarafeuilles": [
+    {
+      "name": "Zone : Plaine des Scarafeuilles",
+      "coord": "Coordonnées : [1,28]",
+      "infos": "Drop items scara ( a vendre pnj ou briser pour runes en tout genre )",
+      "map": "assets/plaine_des_scarafeuilles.png"
+    }
+  ],
+  "lande de sidimote": [
+    {
+      "name": "Zone : Lande de sidimote",
+      "coord": "Coordonnées : [-25,1]",
+      "infos": "Graine de scorbute ( clefs qutan) , diamant",
+      "map": "assets/lande_de_sidimote.png"
+    }
+  ],
+  "cimetière des torturés": [
+    {
+      "name": "Zone : Cimetière des torturés",
+      "coord": "Coordonnées : [-11,38]",
+      "infos": "Linceul ( clefs qutan)",
+      "map": "assets/cimetiere_des_tortures.png"
+    }
+  ],
+  "plage de corail": [
+    {
+      "name": "Zone : Plage de Corail",
+      "coord": "Coordonnées : [-53,0]",
+      "infos": "Ressource mob",
+      "map": "assets/plage_de_corail.png"
+    }
+  ],
+  "tourbière nauséabonde": [
+    {
+      "name": "Zone : Tourbière nauséabonde",
+      "coord": "Coordonnées : [-59,-8]",
+      "infos": "Ressource roissingue et ouassingue, items ouassingue ( gaPA gaPM )",
+      "map": "assets/tourbiere_nauseabonde.png"
+    }
+  ],
+  "plaine de cania": [
+    {
+      "name": "Zone : Plaine de Cania",
+      "coord": "Coordonnées : [-6,-53]",
+      "infos": "Dent de serpentin ( cape ily ) viande kanigrou",
+      "map": "assets/plaine_de_cania.png"
+    }
+  ],
+  "foret de cania": [
+    {
+      "name": "Zone : Foret de Cania",
+      "coord": "Coordonnées : [-6,-46]",
+      "infos": "GaPA ( La doubitch o'boul sur sanglier des plaines ) rune intel pp, vente items pnj",
+      "map": "assets/foret_de_cania.png"
+    }
+  ],
+  "sufokia": [
+    {
+      "name": "Zone : Sufokia",
+      "coord": "Coordonnées : [12,23]",
+      "infos": "Farm cordo/bijoutier/tailleur 60 ( craft item piou ) et farm Kamas bas lvl ( dès le lvl 17 oeil de taupe suffit)",
+      "map": "assets/sufokia.png"
+    }
+  ],
+  "foret abraknyde sombre": [
+    {
+      "name": "Zone : Foret abraknyde sombre",
+      "coord": "Coordonnées : [-14,-13]",
+      "infos": "Ambre abraknyde sombre",
+      "map": "assets/foret_abraknyde_sombre.png"
+    }
+  ]
+};
+
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { Client, EmbedBuilder, AttachmentBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('drop')
+    .setDescription('Réponds avec les maps de drop optimales pour la zone demandée')
+    .addStringOption(option =>
+      option.setName('zone')
+        .setDescription('Nom complet de la zone')
+        .setAutocomplete(true)),
+
+  async autocomplete(interaction) {
+    const focusedValue = interaction.options.getFocused();
+    if (focusedValue.length > 1) {
+      const choices = [];
+      for (const [key, value] of Object.entries(zones)) {
+        choices.push(key.toLowerCase());
+      }
+      const filtered = choices.filter(choice => choice.includes(focusedValue.toLowerCase()));
+      await interaction.respond(
+        filtered.map(choice => ({ name: choice, value: choice }))
+      )
+    }
+  },
+
+  async execute(interaction) {
+    const zone = zones[interaction.options.getString('zone')];
+    if (!zone) {
+      await interaction.reply("Liste de nos zone de drop : https://docs.google.com/spreadsheets/d/1YI_mJL6mVZS7q4hF00qZrLrpvR-b_KKkhZU2jZQDFZc/edit?gid=0#gid=0'");
+      return ;
+    }
+
+    if (zone[0]['name']) {
+      await interaction.reply(zone[0]['name']);
+      await interaction.deleteReply();
+
+      const file = new AttachmentBuilder(zone[0]['map']);
+      const embed = new EmbedBuilder()
+          .setColor(0x0099FF)
+          .setTitle(zone[0]['name'])
+          .setDescription(zone[0]['coord'])
+          .addFields({ name: '\u200b', value: '\u200b' })
+          .addFields({name: 'Ressources a drop :', value: zone[0]['infos']})
+          .setImage('attachment://' + zone[0]['map'].slice(13));
+          await interaction.channel.send({ embeds: [embed], files: [file] });
+    } else {
+      await interaction.reply("Liste de nos zone de drop : https://docs.google.com/spreadsheets/d/1YI_mJL6mVZS7q4hF00qZrLrpvR-b_KKkhZU2jZQDFZc/edit?gid=0#gid=0'");
+    }
+
+  }
+};
